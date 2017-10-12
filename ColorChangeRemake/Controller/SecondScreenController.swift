86f3137f07df2603eeb9fef219da008f9c3587f6
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-10-12 09:08:22 -0600

    Added a new controller

---
+++ ColorChangeRemake/Controller/SecondScreenController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondScreenController.swift
+//  ColorChangeRemake
+//
+//  Created by Wilsher, Ethan on 10/12/17.
+//  Copyright © 2017 Wilsher, Ethan. All rights reserved.
+//
+
+import UIKit
+
+internal class
+SecondScreenController :
+UIViewController
+{
+    
+}
---
+++ ColorChangeRemake/Controller/SecondSwiftController.swift
@@ -0,0 +1,11 @@
+//
+//  SecondSwiftController.swift
+//  ColorChangeRemake
+//
+//  Created by Wilsher, Ethan on 10/12/17.
+//  Copyright © 2017 Wilsher, Ethan. All rights reserved.
+//
+
+import UIKit
+
+
